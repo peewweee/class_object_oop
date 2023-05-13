f95073457f7f2149c24dbd39081df6fc811b7fa9
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-05-14 00:18:56 +0800

    testing class if working

--- class_object_oop.py
+++ class_object_oop.py
@@ -3,9 +3,23 @@
 
 # pseudocode
 # create class tv
+class TV:
+    def __init__(self, TvNumber, TurnOn, TurnOff, GetChannel, SetVolume, GetVolume):
+        self.TvNumber = TvNumber
+        self.TurnOn = TurnOn
+        self.TurnOff = TurnOff
+        self.GetChannel = GetChannel
+        self.SetVolume = SetVolume
+        self.GetVolume = GetVolume
     # method to display objects
+    def show(self):
+        print(self.TvNumber, self.GetChannel, self.GetVolume)
 
 # Test driver program
+def TestTV():
     # create two objects
+    first_tv = TV('TV 1','None', 'None', 30, 'None', 3)
+    first_tv.show()
 
 # call test driver method
+TestTV()
