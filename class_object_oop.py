6f1ea4c014c527be0aa898a70026c94f3bfbf807
Phoebe Gangoso <phoebergangoso@gmail.com>
2023-05-14 00:12:46 +0800

    pseudocode

---
+++ class_object_oop.py
@@ -0,0 +1,11 @@
+# Class and Object - OOP
+# Phoebe Rhone L. Gangoso | BSCPE 1-4
+
+# pseudocode
+# create class tv
+    # method to display objects
+
+# Test driver program
+    # create two objects
+
+# call test driver method
